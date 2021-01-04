ca1ba2741d544719bc731816ee9e6b3155685cdb
Your Name <you@example.com>
2021-01-04 21:41:19 +0800

    20200104-fix-dev-cmd

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.send('Server Running!')
 })
 
 module.exports = router
