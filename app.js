643e64dcdd67abd31afb926eb4b64637d00d9b7d
Your Name <you@example.com>
2021-01-06 07:52:11 +0800

    20210106

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const http = require('http')
+const url = require('url')
 
+const initHttpServer = require('./ws/index')
 const indexRouter = require('./routes/index')
 
 const app = express()
@@ -23,6 +26,8 @@ app.use(function(req, res, next) {
 })
 
 app.use('/', indexRouter)
+
+initHttpServer()
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- ws/index.js
+++ ws/index.js
@@ -1,70 +1,50 @@
 const WebSocket = require('ws')
+const app = require('../app')
 const http = require('http')
+const url = require('url')
 
-function init() {
-  const wss = new WebSocket.Server({
-    noServer: true,
-    port: 8080,
-    perMessageDeflate: {
-      zlibDeflateOptions: {
-        // See zlib defaults.
-        chunkSize: 1024,
-        memLevel: 7,
-        level: 3
-      },
-      zlibInflateOptions: {
-        chunkSize: 10 * 1024
-      },
-      // Other options settable:
-      clientNoContextTakeover: true, // Defaults to negotiated value.
-      serverNoContextTakeover: true, // Defaults to negotiated value.
-      serverMaxWindowBits: 10, // Defaults to negotiated value.
-      // Below options specified as default values.
-      concurrencyLimit: 10, // Limits zlib concurrency for perf.
-      threshold: 1024 // Size (in bytes) below which messages
-      // should not be compressed.
-    }
+const wss1 = new WebSocket.Server({ noServer: true })
+const wss2 = new WebSocket.Server({ noServer: true })
+
+wss1.on('connection', function connection(ws) {
+  // ...
+  console.log('connected to wss1')
+  wss1.on('message', function incoming(message) {
+    console.log('received: %s', message);
   })
-
-  wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-      console.log('received: %s', message)
-    })
+  let i = 0
+  setInterval(() => {
+    const str = 'this is a message: ' + i.toString()
+    ws.send(str)
+    i++
+  }, 1000)
   
-    ws.send('something')
-  })
-
-  const wss1 = new WebSocket.Server({ noServer: true })
-  const wss2 = new WebSocket.Server({ noServer: true })
-
-  wss1.on('connection', function connection(ws) {
-    // ...
-  })
+})
   
-  wss2.on('connection', function connection(ws) {
-    // ...
-  })
-  
-  const server = http.createServer()
+wss2.on('connection', function connection(ws) {
+  // ...
+})
+
+function initHttpServer() {
+  const server = http.createServer(app)
+  server.listen(8082)
 
   server.on('upgrade', function upgrade(request, socket, head) {
-    const pathname = url.parse(request.url).pathname;
-  
-    if (pathname === '/foo') {
+    const pathname = url.parse(request.url).pathname
+    console.log(pathname)
+
+    if (pathname === '/path1') {
       wss1.handleUpgrade(request, socket, head, function done(ws) {
-        wss1.emit('connection', ws, request);
+        wss1.emit('connection', ws, request)
       });
     } else if (pathname === '/bar') {
       wss2.handleUpgrade(request, socket, head, function done(ws) {
-        wss2.emit('connection', ws, request);
+        wss2.emit('connection', ws, request)
       });
     } else {
-      socket.destroy();
+      socket.destroy()
     }
   })
-
-  server.listen(8080)
-  
 }
 
-module.exports = init
+module.exports = initHttpServer
